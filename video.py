933ebf72a45ab47fd6faa81c46c47401a2d5cbdd
aalmoelef2025-cpu <aalmoelef2025@gmail.com>
2026-06-18 14:42:15 +0300

    Delete video.py

--- video.py
+++
@@ -1,7 +0,0 @@
-VIDEO_EXTENSIONS = {"mp4", "webm", "mov", "avi", "mkv"}
-
-def is_video_file(filename):
-    return (
-        "." in filename and
-        filename.rsplit(".", 1)[1].lower() in VIDEO_EXTENSIONS
-    )
